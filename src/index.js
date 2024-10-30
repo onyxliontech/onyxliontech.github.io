b183b432085b8781f8bb76de23bfa599df958522
melak-12 <melakabebeee@gmail.com>
2024-10-31 02:37:07 +0300

    refactor: update BrowserRouter basename to "/portfoliod"

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/portfoliod">
       <App />
-{/*  */}
     </BrowserRouter>
   </React.StrictMode>,
 );
