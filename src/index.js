3c5da2cb8a1781460a8d64036891eabf6c405ef9
melak-12 <melakabebeee@gmail.com>
2024-10-31 01:49:09 +0300

    refactor: enable App component in index.js and remove commented out code

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/">
-      {/* <App /> */}
-      hello
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
